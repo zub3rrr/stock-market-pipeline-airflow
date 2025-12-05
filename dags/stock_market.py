ea9439b1cf7d75ee194d218da3d5d31a9e30a7e5
unknown <xuberswork@gmail.com>
2025-12-05 12:55:26 +0530

    New Task Added | load_to_dw |This will store the output of get_formated_csv to Postgres or any Data Warehouse of your choice using Astro SDK , for further options you can refer astro sdk docs

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -3,9 +3,12 @@ from airflow.sensors.base import PokeReturnValue
 from airflow.hooks.base import BaseHook
 from airflow.operators.python import PythonOperator
 from airflow.providers.docker.operators.docker import DockerOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
 from datetime import datetime
 
-from include.stock_market.tasks import _get_stock_prices , _store_prices , BUCKET_NAME
+from include.stock_market.tasks import _get_stock_prices , _store_prices , _get_formatted_csv , BUCKET_NAME
 
 SYMBOL = "NVDA"
 
@@ -64,7 +67,35 @@ def stock_market():
         }
     )
 
-    is_api_available() >> get_stock_prices >> store_prices >> format_prices
+    get_formatted_csv = PythonOperator(
+        task_id='get_formatted_csv',
+        python_callable=_get_formatted_csv,
+        op_kwargs={
+            'path': '{{ ti.xcom_pull(task_ids="store_prices") }}'
+        }
+    )
+
+    load_to_dw = aql.load_file(
+        task_id='load_to_dw',
+        input_file=File(
+            path=f"s3://{BUCKET_NAME}/{{{{ ti.xcom_pull(task_ids='get_formatted_csv') }}}}",
+            conn_id='minio'
+        ),
+        output_table=Table(
+            name='stock_market',
+            conn_id='postgres',
+            metadata=Metadata(
+                schema='public'
+            )
+        ),
+        load_options={
+            "aws_access_key_id": BaseHook.get_connection('minio').login,
+            "aws_secret_access_key": BaseHook.get_connection('minio').password,
+            "endpoint_url": BaseHook.get_connection('minio').host,
+        }
+    )
+
+    is_api_available() >> get_stock_prices >> store_prices >> format_prices >> get_formatted_csv >> load_to_dw
 
 
 stock_market_dag = stock_market()
